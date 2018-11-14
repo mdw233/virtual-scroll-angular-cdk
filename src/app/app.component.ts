cc3a6ce32b56957b853e9d38bb1e38121fd18088
Matt Withum <MWithum@Paylocity.com>
2018-11-13 21:28:09 -0500

    varied the element heights

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  numbers: number[] = [];
+  numbers: Row[] = [];
 
   constructor() {
     for (let index = 0; index < 10000; index++) {
-      this.numbers.push(index);
+      this.numbers.push(new Row(this.getRndInteger(20, 200), index));
     }
+  }
+
+  private getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
+}
+
+export class Row {
+  constructor(public height, public index) {
   }
 }
